807a9adc593e0e35873d1a636c8461c0be3a5078
Nilo Jr. - LowmaxPC <nilojr.olang@gmail.com>
2025-04-06 22:21:36 +0800

    Update

--- student_enrollment/settings.py
+++ student_enrollment/settings.py
@@ -15,7 +15,13 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
+
+ALLOWED_HOSTS = os.environ.get(
+    "DJANGO_ALLOWED_HOSTS",
+).split(",")
+if DEBUG:
+    ALLOWED_HOSTS.insert(0, "localhost")
+    ALLOWED_HOSTS.insert(0, "127.0.0.1")
 
 # Application definition
 
